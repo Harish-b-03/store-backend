0387332b17a4c98afedb40daa63f064fc6c9271c
Harish B <harish.b@pragmaticplay.com>
2024-11-09 17:45:30 +0530

    initial setup

---
+++ src/main/java/com/example/store_backend/entity/Product.java
@@ -0,0 +1,43 @@
+package com.example.store_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private @Getter @Setter int id;
+
+    private @Getter @Setter String title;
+
+    private @Getter @Setter String description;
+
+    private @Getter @Setter String category;
+
+    private @Getter @Setter float price;
+
+    private @Getter @Setter float rating;
+
+    private @Getter @Setter int availableStock;
+
+    @Override
+    public String toString() {
+        return "Product {" +
+                "id=" + id +
+                ", title='" +  title + '\'' +
+                ", description='" + description + '\'' +
+                ", category='" + category + '\'' +
+                ", price='" + price + '\'' +
+                ", rating='" + rating + '\'' +
+                ", availableStock='" + availableStock +
+                '}';
+    }
+}
