25571e7beaa8afc12c0f01c8fc8b55df996a7a20
Harish B <harish.b@pragmaticplay.com>
2024-11-10 20:55:30 +0530

    feat: db connection and docker added

--- src/main/java/com/example/store_backend/StoreBackendApplication.java
+++ src/main/java/com/example/store_backend/StoreBackendApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StoreBackendApplication {
 
+	//	TODO: In application.properties, get the username and password of the db from env
 	public static void main(String[] args) {
 		SpringApplication.run(StoreBackendApplication.class, args);
 	}
